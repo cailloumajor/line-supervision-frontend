b56c9ee15aa96aab77cb1e0fe7867f6e8e256c33
Arnaud Rocher <arnaud.roche3@gmail.com>
2020-05-06 21:04:30 +0000

    Change page title

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "***REMOVED***"
+      return args
+    })
+  },
   configureWebpack: config => {
     if (process.env.NODE_ENV !== "production") {
       config.devtool = "source-map"
