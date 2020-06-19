3ca11c7fa5cce1b95580462300ea8ba13493e7f6
Arnaud Rocher <arnaud.roche3@gmail.com>
2020-06-19 10:11:58 +0000

    Keep recorded data in a separate component
    No need for the moment to have it in the store

---
+++ web/src/components/RecordedDataGraph.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <div v-if="missingEnvVars.length">
+      <v-alert
+        v-for="(envVar, index) in missingEnvVars"
+        :key="`env-var-${index}`"
+        type="error"
+      >
+        Variable d'environnement {{ envVar }} manquante
+      </v-alert>
+    </div>
+    <div v-else></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { flux, InfluxDB } from "@influxdata/influxdb-client"
+import { Component, Vue } from "vue-property-decorator"
+
+import { automationMapper } from "@/store/modules/automation"
+
+interface RecordedDataSerie {
+  name: string
+  data: [number, number][]
+}
+
+const mapped = Vue.extend({
+  methods: automationMapper.mapMutations(["influxLinkUp", "influxLinkDown"])
+})
+
+@Component
+export default class RecordedDataGraph extends mapped {
+  private fetchInterval!: number
+
+  dataSeries: RecordedDataSerie[] = []
+  envVars: {
+    [key: string]: {
+      varName: string
+      value: string
+      missing: boolean
+    }
+  } = {
+    influxDBName: {
+      varName: "VUE_APP_INFLUX_DB_NAME",
+      value: "",
+      missing: false
+    },
+    influxMeasurement: {
+      varName: "VUE_APP_INFLUX_MEASUREMENT",
+      value: "",
+      missing: false
+    }
+  }
+
+  created(): void {
+    for (const key in this.envVars) {
+      const envVar = process.env[this.envVars[key].varName]
+      if (envVar === undefined) {
+        this.envVars[key].missing = true
+      } else {
+        this.envVars[key].value = envVar
+      }
+    }
+    if (this.missingEnvVars.length) return
+    setTimeout(this.fetchRecordedData, 0)
+    this.fetchInterval = setInterval(this.fetchRecordedData, 60000)
+  }
+
+  beforeDestroy(): void {
+    clearInterval(this.fetchInterval)
+  }
+
+  fetchRecordedData(): void {
+    const result: RecordedDataSerie[] = []
+    const { influxDBName, influxMeasurement } = this.envVars
+    const url = `http://${window.location.host}/influx`
+    const queryAPI = new InfluxDB({ url }).getQueryApi("")
+    const query = flux`\
+      from(bucket: "${influxDBName.value}")
+        |> range(start: -8h)
+        |> filter(fn: (r) => r._measurement == "${influxMeasurement.value}")
+        |> increase()
+    `
+    queryAPI.queryRows(query, {
+      next: (row, tableMeta) => {
+        const o = tableMeta.toObject(row)
+        if (result.findIndex(s => s.name === o._field) < 0) {
+          result.push({ name: o._field, data: [] })
+        }
+        const serieIdx = result.findIndex(s => s.name === o._field)
+        result[serieIdx].data.push([Date.parse(o._time), o._value])
+      },
+      error: err => {
+        this.influxLinkDown()
+        console.error(err)
+      },
+      complete: () => {
+        this.dataSeries = [...result]
+        this.influxLinkUp()
+      }
+    })
+  }
+
+  get missingEnvVars(): string[] {
+    return Object.values(this.envVars)
+      .filter(v => v.missing)
+      .map(v => v.varName)
+  }
+}
+</script>
--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -3,21 +3,14 @@ import Vuex from "vuex"
 import { createStore } from "vuex-smart-module"
 
 import { automation } from "./modules/automation"
-import createInfluxPlugin from "./plugins/influxdb"
 import createWebSocketPlugin from "./plugins/websocket"
 
 Vue.use(Vuex)
 
-const influxDBName = process.env.VUE_APP_INFLUX_DB_NAME
-const influxPlugin = createInfluxPlugin(
-  `http://${window.location.host}/influx`,
-  influxDBName
-)
-
 const wsPlugin = createWebSocketPlugin(`ws://${window.location.host}/ws`)
 
 const store = createStore(automation, {
-  plugins: [influxPlugin, wsPlugin],
+  plugins: [wsPlugin],
   strict: process.env.NODE_ENV !== "production"
 })
 
