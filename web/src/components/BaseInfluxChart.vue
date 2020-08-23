ee8e2905ac9bef7ae177f409623e89777b8a7310
Arnaud Rocher <arnaud.roche3@gmail.com>
2020-08-23 12:00:42 +0000

    Create a common component for InfluxDB based charts

---
+++ web/src/components/BaseInfluxChart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="rounded-lg">
+    <apex-chart
+      :options="chartOptions"
+      :series="chartSeries"
+      :type="chartType"
+    />
+    <v-overlay
+      :dark="null"
+      :light="null"
+      :value="queryError.active"
+      absolute
+      opacity="0"
+      z-index="1"
+    >
+      <v-alert
+        border="bottom"
+        class="caption"
+        colored-border
+        elevation="2"
+        type="error"
+      >
+        Erreur de requête InfluxDB :
+        <div class="font-italic influx-error-text">
+          {{ queryError.text }}
+        </div>
+      </v-alert>
+    </v-overlay>
+  </div>
+</template>
+
+<script lang="ts">
+import { ApexOptions } from "apexcharts"
+import VueApexChart from "vue-apexcharts"
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+export interface QueryError {
+  active: boolean
+  text: string
+}
+
+@Component({
+  components: {
+    "apex-chart": VueApexChart
+  }
+})
+export default class BaseChart extends Vue {
+  @Prop(Object) readonly chartOptions: ApexOptions | undefined
+  @Prop({ type: Array, required: true })
+  readonly chartSeries!: ApexOptions["series"]
+  @Prop({ type: String, required: true }) readonly chartType!: string
+  @Prop({ type: Object, required: true }) readonly queryError!: QueryError
+}
+</script>
