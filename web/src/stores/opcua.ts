aec3bc42a1e31fd2a9eb256431e883ad8e671e39
Arnaud Rocher <arnaud.roche3@gmail.com>
2020-09-03 08:07:30 +0000

    Reset machine metrics when loosing the connection

--- web/src/stores/opcua.ts
+++ web/src/stores/opcua.ts
@@ -1,3 +1,4 @@
+import { toRefs, watch } from "@vue/composition-api"
 import { createStore } from "pinia"
 import { Subject, Subscription, timer } from "rxjs"
 import { delayWhen, retryWhen } from "rxjs/operators"
@@ -10,49 +11,36 @@ import {
   OPCMessage
 } from "./types"
 
-interface StateType {
+type StateType = {
   machinesMetrics: MachineMetrics[]
   lineGlobalParameters: LineGlobalParameters
   opcLinkStatus: LinkStatus
   wsLinkStatus: LinkStatus
 }
 
-const RETRY_DELAY_SEC = 5
-
-const openSubject = new Subject()
-const closeSubject = new Subject()
-const ws$ = webSocket<OPCMessage>({
-  url: `ws://${window.location.host}/ws`,
-  openObserver: openSubject,
-  closeObserver: closeSubject
-}).pipe(
-  retryWhen(errors =>
-    errors.pipe(delayWhen(() => timer(RETRY_DELAY_SEC * 1000)))
-  )
-)
-
-let wsSubscription: Subscription
+const freshMachineMetrics = () =>
+  Array<MachineMetrics>(13).fill({
+    machineState: {
+      cycle: false,
+      alert: false,
+      alarm: false,
+      missingParts: false,
+      saturation: false
+    },
+    counters: {
+      production: 0,
+      toolChangePercent: -1,
+      partControlPercent: -1,
+      bufferFillPercent: -1,
+      cycleTimePercent: 0
+    }
+  })
 
 const useStore = createStore({
   id: "OPC-UA",
 
   state: (): StateType => ({
-    machinesMetrics: Array<MachineMetrics>(13).fill({
-      machineState: {
-        cycle: false,
-        alert: false,
-        alarm: false,
-        missingParts: false,
-        saturation: false
-      },
-      counters: {
-        production: 0,
-        toolChangePercent: -1,
-        partControlPercent: -1,
-        bufferFillPercent: -1,
-        cycleTimePercent: 0
-      }
-    }),
+    machinesMetrics: freshMachineMetrics(),
     lineGlobalParameters: {
       campaignRemaining: 0,
       productionObjective: 0
@@ -73,8 +61,24 @@ const useStore = createStore({
   }
 })
 
+const RETRY_DELAY_SEC = 5
+const openSubject = new Subject()
+const closeSubject = new Subject()
+const ws$ = webSocket<OPCMessage>({
+  url: `ws://${window.location.host}/ws`,
+  openObserver: openSubject,
+  closeObserver: closeSubject
+}).pipe(
+  retryWhen(errors =>
+    errors.pipe(delayWhen(() => timer(RETRY_DELAY_SEC * 1000)))
+  )
+)
+
+let wsSubscription: Subscription
+
 export function useOpcUaStore() {
   const store = useStore()
+  const { opcLinkStatus, wsLinkStatus } = toRefs(store.state)
 
   if (!wsSubscription) {
     openSubject.subscribe(() => {
@@ -104,6 +108,12 @@ export function useOpcUaStore() {
       }
     })
   }
+
+  watch([opcLinkStatus, wsLinkStatus], statuses => {
+    if (!statuses.every(status => status === LinkStatus.Up)) {
+      store.state.machinesMetrics = freshMachineMetrics()
+    }
+  })
 
   return store
 }
