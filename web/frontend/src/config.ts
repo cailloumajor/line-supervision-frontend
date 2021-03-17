1280fd78fc06d95519eab381f0ed261f06ec43cd
Arnaud Rocher <arnaud.roche3@gmail.com>
2021-03-17 08:39:32 +0000

    Use a map instead of an object

--- web/frontend/src/config.ts
+++ web/frontend/src/config.ts
@@ -4,13 +4,13 @@ const cookies = new Cookies()
 
 type CookieName = "centrifugo_token" | "influx_db_name"
 
-const cookiesDefaultValues: { [key in CookieName]: string } = {
-  ["centrifugo_token"]: process.env.VUE_APP_CENTRIFUGO_TOKEN,
-  ["influx_db_name"]: process.env.VUE_APP_INFLUX_DB_NAME
-}
+const cookiesDefaultValues = new Map<CookieName, string>([
+  ["centrifugo_token", process.env.VUE_APP_CENTRIFUGO_TOKEN],
+  ["influx_db_name", process.env.VUE_APP_INFLUX_DB_NAME]
+])
 
 if (process.env.NODE_ENV === "development") {
-  for (const [key, value] of Object.entries(cookiesDefaultValues)) {
+  for (const [key, value] of cookiesDefaultValues) {
     cookies.set(key, value, { sameSite: "strict" })
   }
 }
