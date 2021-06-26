ed3c8d12c1326c179748f4870399f1a372fb6953
Arnaud Rocher <arnaud.roche3@gmail.com>
2021-06-26 11:21:14 +0000

    Add a logo endpoint to UI configuration API service

--- ui-config/src/main.rs
+++ ui-config/src/main.rs
@@ -1,6 +1,7 @@
-use std::env;
 use std::fs;
+use std::path::{Path, PathBuf};
 
+use actix_files::NamedFile;
 use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use anyhow::{Context, Result};
 use env_logger::Builder;
@@ -8,31 +9,41 @@ use log::LevelFilter;
 
 use ui_config::{replace_env_vars, toml_to_json};
 
-const TEMPLATE_ENV_VAR: &str = "CONFIG_TEMPLATE_PATH";
+const DATA_DIR: &str = "config_data";
+const CONFIG_FILE: &str = "config.toml";
+const LOGO_FILE: &str = "logo.png";
 
+fn file_path(file: &str) -> PathBuf {
+    Path::new(DATA_DIR).join(file)
+}
 struct AppState {
     config_json: String,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> impl Responder {
+#[get("/config")]
+async fn config(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok()
         .content_type("application/json")
         .body(&data.config_json)
 }
 
+#[get("/logo")]
+async fn logo() -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open(file_path(LOGO_FILE))?)
+}
+
 #[actix_web::main]
 async fn main() -> Result<()> {
     Builder::new().filter_level(LevelFilter::Info).init();
-    let template_path = env::var(TEMPLATE_ENV_VAR).unwrap_or("config.toml".into());
-    let raw_toml = fs::read_to_string(&template_path)
-        .with_context(|| format!("Failed reading {}", template_path))?;
+    let template_path = &file_path(CONFIG_FILE);
+    let raw_toml = fs::read_to_string(template_path)
+        .with_context(|| format!("Failed reading {}", template_path.display()))?;
     let raw_json = toml_to_json(&raw_toml)
-        .with_context(|| format!("failed to convert {} to JSON", template_path))?;
+        .with_context(|| format!("failed to convert {} to JSON", template_path.display()))?;
     let config_json = replace_env_vars(&raw_json).with_context(|| {
         format!(
             "failed to expand environment variables after converting {} to JSON",
-            template_path
+            template_path.display()
         )
     })?;
     let state = web::Data::new(AppState { config_json });
@@ -42,7 +53,8 @@ async fn main() -> Result<()> {
         App::new()
             .app_data(state.clone())
             .wrap(logger)
-            .service(index)
+            .service(config)
+            .service(logo)
     })
     .bind("0.0.0.0:8080")?
     .run()
